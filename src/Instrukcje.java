1d486e88e6a60b3bcbe6c936e5f5181818316504
Sandra Madalińska-Piórkowska <sandra@netbiznes.com>
2020-12-23 18:14:44 +0100

    Dodanie pliku

---
+++ src/Instrukcje.java
@@ -0,0 +1,11 @@
+
+public class Instrukcje {
+
+	String pozycja;
+	String wartosc;
+
+	public Instrukcje(String pozycja, String wartosc) {
+		this.pozycja = pozycja;
+		this.wartosc = wartosc;
+	}
+}
