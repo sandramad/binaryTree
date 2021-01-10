5d13f01a00359f92f9259fc228fb99c059f5b792
Sandra Madalińska-Piórkowska <sandra@netbiznes.com>
2021-01-10 12:29:40 +0100

    Dodanie sprawdzania ostatniego chara

---
+++ src/MainChar.java
@@ -0,0 +1,71 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Wezel {
+	public Character wartosc;
+	public Wezel lewa;
+	public Wezel prawa;
+}
+
+public class MainChar {
+
+	Wezel korzen = new Wezel();
+	Character chTemp = 'a';
+
+	private void budujDrzewo(Wezel wezel, String sciezka, char wartosc) {
+		if (sciezka.length() == 0)
+			wezel.wartosc = wartosc;
+		else if (sciezka.charAt(0) == 'l') {
+			if (wezel.lewa == null)
+				wezel.lewa = new Wezel();
+			budujDrzewo(wezel.lewa, sciezka.substring(1), wartosc);
+		} else {
+			if (wezel.prawa == null)
+				wezel.prawa = new Wezel();
+			budujDrzewo(wezel.prawa, sciezka.substring(1), wartosc);
+		}
+	}
+
+	private void skanujPlik() {
+		try (Scanner scan = new Scanner(new File("tree.txt"))) {
+			while (scan.hasNextLine()) {
+				String wiersz = scan.nextLine().toLowerCase();
+				Character wartosc = wiersz.charAt(wiersz.length() - 1);
+				String sciezka = "";
+				if (wiersz.length() > 2)
+					sciezka = wiersz.substring(0, wiersz.length() - 2);
+				budujDrzewo(korzen, sciezka, wartosc);
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String znajdzNajstarszy(Wezel wezel, String najstarszy, StringBuilder slowo) {
+		slowo.append(wezel.wartosc);
+		if (wezel.lewa == null && wezel.prawa == null) {
+			if (chTemp < wezel.wartosc) {
+				chTemp = wezel.wartosc;
+				najstarszy = new StringBuilder(slowo).reverse().toString();
+			} else if (chTemp == wezel.wartosc) {
+				String tmp = new StringBuilder(slowo).reverse().toString();
+				najstarszy = (najstarszy.compareTo(tmp) > 0) ? najstarszy : tmp;
+			}
+		} else {
+			if (wezel.lewa != null)
+				najstarszy = znajdzNajstarszy(wezel.lewa, najstarszy, slowo);
+			if (wezel.prawa != null)
+				najstarszy = znajdzNajstarszy(wezel.prawa, najstarszy, slowo);
+		}
+		slowo.deleteCharAt(slowo.length() - 1);
+		return najstarszy;
+	}
+
+	public static void main(String[] args) {
+		MainChar main = new MainChar();
+		main.skanujPlik();
+		System.out.println(main.znajdzNajstarszy(main.korzen, "", new StringBuilder()));
+	}
+
+}
